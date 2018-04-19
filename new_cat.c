a938faf446ed302df51efe700da4cede0f0133d3
Lee <leep9@debian-9.3.0-template>
2018-04-19 11:36:05 -0700

    Added more comments as well as codes for the program to fail if the file does not exist, working on to fix the program so it repeats what user types on stdin back to stdout when there's no argument

--- new_cat.c
+++ new_cat.c
@@ -8,42 +8,69 @@ int main(int argc, char *argv[])
 {
 	//delcare variables
 	FILE *stream;
-	char *lineptr = NULL;
+	char *line = NULL;
 	size_t n = 0;
 	ssize_t  nread;
 	int character;
+
+	//If the user passed in > 2 arguments,
+	//prints out a error message
+	if(argc > 2)
+	{
+		fprintf(stderr, "The arguments are > 2, please retry");
+		exit(EXIT_FAILURE);
+	}
 
 	//This call to fopen() takes the path to a file to open
 	//(the argument is argv[1], the filename provided by 
 	//the user), followed by the access mode (the "r" argument
 	//means read only). fopen returns a FILE * (pointer to
 	//a stream representing the open file)
-	if(stream = fopen(argv[1],"r"))
-	{
+	else if(stream = fopen(argv[1],"r"))
+	{	
 		//This call to getline() reads an entire line from stream
-		//since *lineptr is set to NULL and *n is set to 0
+		//since *line pointer is set to NULL and *n is set to 0
 		//before the call, getline() will allocate a buffer
 		//for storing the line
-		while((nread = getline(&lineptr, &n, stream)) != -1)
+		while((nread = getline(&line, &n, stream)) != -1)
 		{
+			//This call to fwrite() writes items of data 
+			//each size bytes long, to stdout pointed to
+			//by stream, obtaining them from the location
+			//given by ptr line
 			fwrite(line, nread, 1, stdout);
 		} 
 		
 		//This call to fclose() flushes the stream pointed to 
 		//by the stream and closes the underlying file descriptor.
-		//When successful, 0 is returned. Program exits
+		//When successful, 0 is returned, program exits.
 		fclose(stream);
 		exit(EXIT_SUCCESS);
 	}
+	//If the file does not exist or fopen fails
+	//print an error message
+	else if((stream = fopen(argv[1],"r")) == NULL)
+	{
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+	//If there is no second argument
+	//repate what the user types back to stdout
 	else
 	{
+		//getchar() reads the next character from
+		//stdin and returns it. The while loop
+		//makes it read to the end of file until 
+		//user hits CRTL+D to indicate EOF
 		character = getchar();
 		while(character != EOF)
 		{
+			//putchar() writes the character
+			//and output it to stdout.
 			putchar(character);
 			character = getchar();
-		
 		}
+
 		return 0;
 	}
 }
